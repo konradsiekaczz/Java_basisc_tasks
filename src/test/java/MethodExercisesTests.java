bbf55b7bcb91371601b61aa436e410a7d8fcd717
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 20:58:50 +0100

    adding test class

---
+++ src/test/java/MethodExercisesTests.java
@@ -0,0 +1,13 @@
+import com.konrad.MethodExercises;
+import org.junit.gen5.api.BeforeAll;
+import org.junit.gen5.api.Test;
+
+public class MethodExercisesTests {
+
+    private MethodExercises methodExercises;
+
+    @BeforeAll
+    public void setUp(){
+        methodExercises = new MethodExercises();
+    }
+}
