3999cde2d21dc556d2c21b2974e41eab8e592cd5
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 18:52:15 +0100

    add method which return age of user

--- src/com/konrad/Main.java
+++ src/com/konrad/Main.java
@@ -1,8 +1,16 @@
 package com.konrad;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner;
 
     public static void main(String[] args) {
+    }
 
+    /*This method accepts year of birth as an argument and returns age.*/
+    private static int returnMyCurrentYear(int birthYear){
+        return LocalDate.now().getYear() - birthYear;
     }
 }
