6bf8f2c8ed36c20878a52830257687c05d2a33c1
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 21:33:04 +0100

    changing access modifiers for methods

--- src/main/java/com/konrad/MethodExercises.java
+++ src/main/java/com/konrad/MethodExercises.java
@@ -19,34 +19,34 @@ public class MethodExercises {
     }
 
     /*This method prints basics calculating between two arguments.*/
-    private static void calculate(int x, int y) {
+    public static void calculate(int x, int y) {
         System.out.println(x + y);
         System.out.println(x - y);
         System.out.println(x * y);
     }
 
     /*This method checks if number is even.*/
-    private static boolean isEven(double number) {
+    public static boolean isEven(double number) {
         return number % 2 == 0;
     }
 
     /*This method checks if number is divide by 3 and 5.*/
-    private static boolean isDivideBy3Or5(double num) {
+    public static boolean isDivideBy3Or5(double num) {
         return num % 3 == 0 && num % 5 == 0;
     }
 
     /*This method returns a number raised to the power of 3.*/
-    private static double thirdPower(double num) {
+    public static double thirdPower(double num) {
         return Math.pow(num, 3);
     }
 
     /*This method returns a square root of number.*/
-    private static double squareRoot(int num) {
+    public static double squareRoot(int num) {
         return Math.sqrt(num);
     }
 
     /*This method checks if we can build a right triangle using three number as an arguments.*/
-    private static boolean canMakeTriangle(int a, int b, int c) {
+    public static boolean canMakeTriangle(int a, int b, int c) {
         return Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2);
     }
 }
