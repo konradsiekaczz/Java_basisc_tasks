8b810be5913aa76d82d350124360bf0d14559669
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 18:58:55 +0100

    adding method which return the first name

--- src/com/konrad/Main.java
+++ src/com/konrad/Main.java
@@ -4,13 +4,18 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner;
 
     public static void main(String[] args) {
+        System.out.println(returnName("Konrad"));
     }
 
-    /*This method accepts year of birth as an argument and returns age.*/
+    /*This method accepts year of birth as an argument and returns current age after calculating.*/
     private static int returnMyCurrentYear(int birthYear){
         return LocalDate.now().getYear() - birthYear;
+    }
+
+    /*This method accepts name as an argument and returns it.*/
+    private static String returnName(String s){
+        return s;
     }
 }
