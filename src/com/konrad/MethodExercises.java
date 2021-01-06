f3341d4422e0097c017afe522bab22f68ee37a1d
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 19:21:04 +0100

    adding method which checks if number is divide by 3 and 5

--- src/com/konrad/MethodExercises.java
+++ src/com/konrad/MethodExercises.java
@@ -9,24 +9,30 @@ public class MethodExercises {
     }
 
     /*This method accepts year of birth as an argument and returns current age after calculating.*/
-    private static int returnMyCurrentYear(int birthYear){
+    private static int returnMyCurrentYear(int birthYear) {
         return LocalDate.now().getYear() - birthYear;
     }
 
     /*This method accepts name as an argument and returns it.*/
-    private static String returnName(String s){
+    private static String returnName(String s) {
         return s;
     }
 
     /*This method prints basics calculating between two arguments.*/
-    private static void calculate(int x, int y){
+    private static void calculate(int x, int y) {
         System.out.println(x + y);
         System.out.println(x - y);
         System.out.println(x * y);
     }
 
     /*This method checks if number is even.*/
-    private static boolean isEven(double number){
-        return number % 2 ==0;
+    private static boolean isEven(double number) {
+        return number % 2 == 0;
     }
+
+    /*This method checks if number is divide by 3 and 5.*/
+    private static boolean isDivideBy3Or5(double num) {
+        return num % 3 == 0 && num % 5 == 0;
+    }
+
 }
