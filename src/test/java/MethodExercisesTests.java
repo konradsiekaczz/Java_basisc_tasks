f39767646b50a5fbc8140bfae30576193450f447
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-06 21:43:11 +0100

    adding test for isEven method

--- src/test/java/MethodExercisesTests.java
+++ src/test/java/MethodExercisesTests.java
@@ -16,4 +16,15 @@ public class MethodExercisesTests {
     public void should_return_31() {
         Assert.assertEquals(31, MethodExercises.returnMyCurrentAge(1990));
     }
+
+    @Test
+    public void should_return_name(){
+        Assert.assertEquals("Konrad", MethodExercises.returnName("Konrad"));
+    }
+
+    @Test
+    public void should_return_true_for_even_number(){
+        Assert.assertTrue(String.valueOf(true),MethodExercises.isEven(4));
+    }
+
 }
