d0c417867042c6988cea67d1d7f67a4b32abb6ed
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-07 22:06:08 +0100

    adding methods which check if number is even or odd

--- src/main/java/com/konrad/MyNumber.java
+++ src/main/java/com/konrad/MyNumber.java
@@ -1,4 +1,18 @@
 package com.konrad;
 
 public class MyNumber {
+    private long num;
+
+    public MyNumber(long num) {
+        this.num = num;
+    }
+
+    public boolean isOdd() {
+        return this.num % 2 != 0;
+    }
+
+    public boolean isEven() {
+        return this.num % 2 == 0;
+    }
+
 }
