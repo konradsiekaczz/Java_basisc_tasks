627bd211b149502aef820c25dfe475b3b29d25ac
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-07 21:42:33 +0100

    refactoring code

--- src/test/java/MethodExercisesTests.java
+++ src/test/java/MethodExercisesTests.java
@@ -18,38 +18,37 @@ public class MethodExercisesTests {
     }
 
     @Test
-    public void should_return_name(){
+    public void should_return_name() {
         Assert.assertEquals("Konrad", MethodExercises.returnName("Konrad"));
     }
 
     @Test
-    public void should_return_true_for_even_number(){
-        Assert.assertTrue(String.valueOf(true),MethodExercises.isEven(4));
+    public void should_return_true_for_even_number() {
+        Assert.assertTrue(String.valueOf(true), MethodExercises.isEven(4));
     }
 
     @Test
-    public void should_return_false_for_number_not_divisible_by_three_and_five(){
-        Assert.assertFalse(String.valueOf(false),MethodExercises.isDivisibleByThreeAndFive(20));
+    public void should_return_false_for_number_not_divisible_by_three_and_five() {
+        Assert.assertFalse(String.valueOf(false), MethodExercises.isDivisibleByThreeAndFive(20));
     }
 
     @Test
-    public void should_return_true_for_number_divisible_by_three_and_five(){
-        Assert.assertTrue(String.valueOf(true),MethodExercises.isDivisibleByThreeAndFive(15));
+    public void should_return_true_for_number_divisible_by_three_and_five() {
+        Assert.assertTrue(String.valueOf(true), MethodExercises.isDivisibleByThreeAndFive(15));
     }
 
     @Test
-    public void should_return_nine_for_three_to_the_third_power(){
-        Assert.assertEquals(27, MethodExercises.thirdPower(3),0);
+    public void should_return_nine_for_three_to_the_third_power() {
+        Assert.assertEquals(27, MethodExercises.thirdPower(3), 0);
     }
 
     @Test
-    public void should_return_two_for_the_square_root_of_four(){
-        Assert.assertEquals(2, MethodExercises.squareRoot(4),0);
+    public void should_return_two_for_the_square_root_of_four() {
+        Assert.assertEquals(2, MethodExercises.squareRoot(4), 0);
     }
 
     @Test
-    public void should_return_the_truth_for_the_sides_from_which_a_triangle_can_be_formed(){
-        Assert.assertTrue(String.valueOf(true), MethodExercises.canMakeTriangle(3,4,5));
+    public void should_return_the_truth_for_the_sides_from_which_a_triangle_can_be_formed() {
+        Assert.assertTrue(String.valueOf(true), MethodExercises.canMakeTriangle(3, 4, 5));
     }
-
 }
