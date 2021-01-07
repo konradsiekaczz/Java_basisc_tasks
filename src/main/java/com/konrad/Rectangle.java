ca9f86419cc27b553800333edb72889067894f16
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-07 21:58:12 +0100

    adding method which calculate area, perimeter and diagonal for rectangle

--- src/main/java/com/konrad/Rectangle.java
+++ src/main/java/com/konrad/Rectangle.java
@@ -1,5 +1,23 @@
 package com.konrad;
 
 public class Rectangle {
+    private double length;
+    private double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * length + 2 * width;
+    }
+
+    public double calculateDiagonal(){
+        return Math.sqrt(Math.pow(2,length) + Math.pow(2, width));
+    }
 }
