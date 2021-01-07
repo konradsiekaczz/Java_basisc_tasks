683fc4c0158eb41a9271d3b32ea34375455e0504
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-07 22:19:23 +0100

    adding method which calculate pow, sqrt, adding, subtract

--- src/main/java/com/konrad/MyNumber.java
+++ src/main/java/com/konrad/MyNumber.java
@@ -8,11 +8,27 @@ public class MyNumber {
     }
 
     public boolean isOdd() {
-        return this.num % 2 != 0;
+        return num % 2 != 0;
     }
 
     public boolean isEven() {
-        return this.num % 2 == 0;
+        return num % 2 == 0;
+    }
+
+    public long sqrt(){
+        return (long) Math.sqrt(num);
+    }
+
+    public MyNumber pow(MyNumber x){
+        return new MyNumber((long) Math.pow(num,x.num));
+    }
+
+    public MyNumber add(MyNumber x){
+        return new MyNumber(num + x.num);
+    }
+
+    public MyNumber subtract(MyNumber x){
+        return new MyNumber(num - x.num);
     }
 
 }
