c0c5e21a3417508c5ddf44f90d3e9fd276784b31
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-08 22:08:55 +0100

    adding Human test class with test for constructor

---
+++ src/test/java/HumanTests.java
@@ -0,0 +1,13 @@
+import com.konrad.Human;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HumanTests {
+    private Human human;
+
+    @Test
+    public void should_create_human(){
+        human = new Human(30,74,178,"Konrad","male");
+        Assert.assertEquals(30,human.getAge());
+    }
+}
