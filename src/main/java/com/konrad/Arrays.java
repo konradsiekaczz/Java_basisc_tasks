2ca876d785a007c9b22fb510eff056f872829680
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-08 19:54:58 +0100

    adding method to reverse elements in array

--- src/main/java/com/konrad/Arrays.java
+++ src/main/java/com/konrad/Arrays.java
@@ -6,22 +6,22 @@ import java.util.Collections;
 public class Arrays {
 
     public static void main(String[] args) {
-        Object[]array = new Object[]{new Arrays(),new Human(2,2,2,"k","e")};
+        Object[] array = new Object[]{new Arrays(), new Human(2, 2, 2, "k", "e")};
 
         reverseElements(array);
 
         System.out.println(java.util.Arrays.toString(array));
     }
 
-    public static String[] returnLetters(){
-        return  new String[]{"A","B","C","D","E"};
+    public static String[] returnLetters() {
+        return new String[]{"A", "B", "C", "D", "E"};
     }
 
-    public static void reverseElements(Object[] arrays){
-        for (int i = 0; i < arrays.length/2; i++) {
+    public static void reverseElements(Object[] arrays) {
+        for (int i = 0; i < arrays.length / 2; i++) {
             Object temp = arrays[i];
             arrays[i] = arrays[arrays.length - i - 1];
-            arrays[arrays.length -i - 1] = temp;
+            arrays[arrays.length - i - 1] = temp;
         }
     }
 }
