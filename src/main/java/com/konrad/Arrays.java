30c467df8eb279988b3707485e43177f7a558ac6
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-08 22:08:13 +0100

    adding description to method

--- src/main/java/com/konrad/Arrays.java
+++ src/main/java/com/konrad/Arrays.java
@@ -13,10 +13,12 @@ public class Arrays {
         System.out.println(java.util.Arrays.toString(array));
     }
 
+    /*This method return five first letter of alphabet*/
     public static String[] returnLetters() {
         return new String[]{"A", "B", "C", "D", "E"};
     }
 
+    /*This method reverse elements in array*/
     public static void reverseElements(Object[] arrays) {
         for (int i = 0; i < arrays.length / 2; i++) {
             Object temp = arrays[i];
