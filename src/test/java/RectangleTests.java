e03399626075bcb094f1e2cdd0b0244e6075d8f8
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-18 19:25:07 +0100

    adding constructor

--- src/test/java/RectangleTests.java
+++ src/test/java/RectangleTests.java
@@ -1,2 +1,12 @@
+import com.konrad.objects.Rectangle;
+import org.junit.Before;
+
 public class RectangleTests {
+
+    private Rectangle rectangle;
+
+    @Before
+    public void setUp(){
+        rectangle = new Rectangle(2,4);
+    }
 }
