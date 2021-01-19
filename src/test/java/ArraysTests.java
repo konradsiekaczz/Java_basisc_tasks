00dc42d0f1f8a0d0d6fa80684bb8be0c1ab3dee2
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-19 12:23:16 +0100

    adding test for returnLetters method

--- src/test/java/ArraysTests.java
+++ src/test/java/ArraysTests.java
@@ -1,2 +1,12 @@
+import com.konrad.arrays.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArraysTests {
+
+    @Test
+    public void should_return_array_with_five_first_letter_from_alphabet() {
+        String[] expected = new String[]{"A", "B", "C", "D", "E"};
+        Assert.assertArrayEquals(expected, Arrays.returnLetters());
+    }
 }
