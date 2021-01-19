ef73e863bed847347944715932fbd564b1a86071
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-19 12:51:34 +0100

    adding method which print number from 0 to n

--- src/main/java/com/konrad/loops_and_conditional_statements/LoopsAndConditionalStatements.java
+++ src/main/java/com/konrad/loops_and_conditional_statements/LoopsAndConditionalStatements.java
@@ -1,4 +1,30 @@
 package com.konrad.loops_and_conditional_statements;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class LoopsAndConditionalStatements {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        printFrom0ToNFromInput();
+    }
+
+    public static void printFrom0ToNFromInput() throws IOException {
+        while (true) {
+            try {
+                String inputStr = br.readLine();
+                int inputInt = Integer.parseInt(inputStr);
+
+                for (int i = 0; i <= inputInt; i++)
+                    System.out.println(i);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong typ of input, please try again!");
+            }
+        }
+    }
 }
