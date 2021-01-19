9148a8ede4de3e1bc642822c65e7e4feeb8b0938
konradsiekaczz <siekaczkonrad@gmail.com>
2021-01-19 13:15:37 +0100

    adding method which print number from 0 to n without taking input from user

--- src/main/java/com/konrad/loops_and_conditional_statements/LoopsAndConditionalStatements.java
+++ src/main/java/com/konrad/loops_and_conditional_statements/LoopsAndConditionalStatements.java
@@ -3,14 +3,13 @@ package com.konrad.loops_and_conditional_statements;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class LoopsAndConditionalStatements {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         printFrom0ToNFromInput();
+        printFrom0ToN(2);
     }
 
     public static void printFrom0ToNFromInput() throws IOException {
@@ -25,6 +24,12 @@ public class LoopsAndConditionalStatements {
             } catch (NumberFormatException e) {
                 System.out.println("Wrong typ of input, please try again!");
             }
+        }
+    }
+
+    public static void printFrom0ToN(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
         }
     }
 }
